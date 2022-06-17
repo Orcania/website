580d53d479b2cfb7798267c0ccc09b44f12beabc
loque18 <madridx2010@gmail.com>
2022-06-17 14:41:45 -0500

    feat: added the services rhombus shape

--- pages/services/tilted-square-box/index.js
+++ pages/services/tilted-square-box/index.js
@@ -1,14 +1,13 @@
 import styles from './styles.module.scss';
 
+const { tilted_box, box_content, text } = styles;
 const TiltedSquareBox = ({ title, subtitle, icon }) => {
-    const { tilted_box, box_content } = styles;
-
     return (
-        <div className={`box ${tilted_box}`}>
+        <div className={` ${tilted_box}`}>
             <div className={`${box_content} has-text-centered is-size-7-mobile`}>
                 <i className={`${icon} has-text-black is-size-4`} />
                 <h1 className="title has-text-black is-size-4 pt-4">{title}</h1>
-                <p className="subtitle has-text-black is-size-6 pt-4">{subtitle}</p>
+                <p className={`subtitle has-text-black is-size-6 pt-4 ${text}`}>{subtitle}</p>
             </div>
         </div>
     );
