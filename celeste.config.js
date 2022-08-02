0daa9f0907d2f4b39ed6b60a99ab2132be19c24d
Bechara Maalouf <tiredzone25@gmail.com>
2022-08-02 14:23:38 +0300

    feat: updated abis and smart contracts addresses

--- celeste.config.js
+++ celeste.config.js
@@ -1,5 +1,6 @@
 const ocamint = require('./src/static/abis/ocamint.json');
-const oca_referral_mint = require('./src/static/abis/oca_mint_referral.json');
+const oca = require('./src/static/abis/oca.json');
+// const oca_referral_mint = require('./src/static/abis/oca_mint_referral.json');
 const erc721 = require('./src/static/abis/erc721.json');
 
 module.exports = {
@@ -12,12 +13,17 @@ module.exports = {
             chainId: 56,
             url: 'https://bsc-dataseed.binance.org/',
         },
+        BOBA: {
+            chainId: 288,
+            url: 'https://mainnet.boba.network/',
+        },
     },
     smartContracts: [
         { key: 'OCAMINT', abi: ocamint },
         { key: 'TRAF', abi: erc721 },
-        { key: 'OCA_REFERRAL_MINT', abi: oca_referral_mint },
+        { key: 'OCA', abi: oca },
 
+        // { key: 'OCA_REFERRAL_MINT', abi: oca_referral_mint },
         // { key: 'DEX_FETCH', abi: dex_fetch, address: '' },
         // { key: 'ERC20', abi: erc20, address: '' },
         // { key: 'ERC20EXTENDED', abi: erc20extended, address: '' },
@@ -25,23 +31,22 @@ module.exports = {
     ],
     addressBook: {
         OCAMINT: {
-            1: '0x4c4BAB8b80785272d0089D76E85828f9a70FdbC2',
-            56: '0x4c4BAB8b80785272d0089D76E85828f9a70FdbC2',
-            // 137: '0x4c4BAB8b80785272d0089D76E85828f9a70FdbC2',
-            // 1666600000: '0x4c4BAB8b80785272d0089D76E85828f9a70FdbC2',
+            1: '0xb90A6769414cB32234915e25a38b842287067532',
+            56: '0xb90A6769414cB32234915e25a38b842287067532',
+            288: '0xb90A6769414cB32234915e25a38b842287067532',
         },
         OCA: {
-            1: '0x8AeB42F7b4204C956c51907C89639E3446a787Ea',
-            56: '0x8AeB42F7b4204C956c51907C89639E3446a787Ea',
-            // 137: '0x8AeB42F7b4204C956c51907C89639E3446a787Ea',
-            // 1666600000: '0x8AeB42F7b4204C956c51907C89639E3446a787Ea',
+            1: '0x3f8C3b9F543910F611585E3821B00af0617580A7',
+            56: '0x3f8C3b9F543910F611585E3821B00af0617580A7',
+            288: '0x3f8C3b9F543910F611585E3821B00af0617580A7',
         },
-        OCA_REFERRAL_MINT: {
-            1: '0x4E61b3C4aCD2C0Aac19cdf28c8D0e4036a2e5338',
-            56: '0xB965a6429dfeE72CD0d5EBd00a7D0820bFCca94C',
-            // 137: '0x6200D0C05B287785c7193a31dDcfDB15c8e4BDd9',
-            // 1666600000: '0x2455Fca2834ee8dbeb7186A10A79305ED8395733',
-        },
+
+        // OCA_REFERRAL_MINT: {
+        //     1: '0x4E61b3C4aCD2C0Aac19cdf28c8D0e4036a2e5338',
+        //     56: '0xB965a6429dfeE72CD0d5EBd00a7D0820bFCca94C',
+        //     288: '0xB965a6429dfeE72CD0d5EBd00a7D0820bFCca94C',
+        // },
+
         TRAF: {
             1: '0x81f296145C5555d7b4B426D97E4e2260c017F5d0',
         },
